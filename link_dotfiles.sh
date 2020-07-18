22cbe5bbfab7a0a8ee3e5f41a4b5e696a3b389e4
nana <haysato.personal@gmail.com>
2020-07-18 19:52:13 +0900

    rename

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/autoload ~/.vim
+ln -sf ~/dotfiles/vim/plugged ~/.vim
 ln -sf ~/dotfiles/doom-emacs/init.el ~/.doom.d/init.el
 ln -sf ~/dotfiles/doom-emacs/config.el ~/.doom.d/config.el
 ln -sf ~/dotfiles/doom-emacs/packages.el ~/.doom.d/packages.el
