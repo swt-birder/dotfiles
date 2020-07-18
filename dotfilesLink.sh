96ec89ad86c737394154e5449e361075c8642722
nana <haysato.personal@gmail.com>
2020-07-18 14:52:38 +0900

    rename

--- dotfilesLink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ln -sf ~/dotfiles/doom-emacs/init.el ~/.doom.d/init.el
-ln -sf ~/dotfiles/doom-emacs/config.el ~/.doom.d/config.el
-ln -sf ~/dotfiles/doom-emacs/packages.el ~/.doom.d/packages.el
-ln -sf ~/dotfiles/doom-emacs/emacs.d/init.el ~/.emacs.d/init.el
