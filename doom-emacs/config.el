c7725c50e5ea2f34cd3a0783e1209b089de497d5
nana <haysato.personal@gmail.com>
2020-07-18 18:20:02 +0900

    wakaran doom-modeline

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -20,34 +20,6 @@
   (doom-themes-neotree-config)
   (doom-themes-org-config))
 
-(use-package doom-themes
-  :custom
-  (doom-themes-enable-italic t)
-  (doom-themes-enable-bold t)
-  :custom-face
-  ;; (vertical-bar   (doom-darken base5 0.4))
-  ;; (doom-darken bg 0.4)
-  :config
-  (load-theme 'doom-dracula t)
-  (doom-themes-neotree-config)
-  (doom-themes-org-config)
-  ;; Modeline
-  (use-package doom-modeline
-    :custom
-    (doom-modeline-buffer-file-name-style 'truncate-with-project)
-    (doom-modeline-icon t)
-    (doom-modeline-major-mode-icon nil)
-    (doom-modeline-minor-modes nil)
-    :hook
-    (after-init . doom-modeline-mode)
-    :config
-    (set-cursor-color "cyan")
-    (line-number-mode 0)
-    (column-number-mode 0)
-    (doom-modeline-def-modeline 'main
-      '(bar workspace-number window-number evil-state god-state ryo-modal xah-fly-keys matches buffer-info remote-host buffer-position parrot selection-info)
-      '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker))))
-
 (use-package beacon
   :custom
   (beacon-color "yellow")
