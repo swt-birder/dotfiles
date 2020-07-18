1f1b6a0d25c10fca860c68b3dc5b8e172989479e
nana <haysato.personal@gmail.com>
2020-07-18 16:00:16 +0900

    fix name in config.el

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "swt-birder"
+      user-mail-address "swt-birder@example.com")
 
 (use-package beacon
   :custom
